8d9ecc671900d3322b33daf51ad313ed0bd18c10
rayah97 <harutyunyan.raya97@gmail.com>
2022-12-25 02:22:25 +0400

    added new version

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    env = os.environ.get('FLASK_ENV', 'development')
+    return f'Hello, Docker! Running in {env} mode.'
+
+if __name__ == '__main__':
+    app.run()
